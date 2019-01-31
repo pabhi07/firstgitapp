80bc04006f0dd9f2b5011429e73c95cf1cb6b5b1
Abhi <abhishekpatangay@yahoo.com>
2019-01-31 23:43:44 +0530

    initial commit

---
+++ WebApisample/Controllers/EmployeesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace WebApisample.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (NorthwindEntities entities = new NorthwindEntities())
+            {
+                return entities.Employees.ToList();
+
+            }
+        }
+        public Employee Get(int id)
+        {
+            using (NorthwindEntities  entities = new NorthwindEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.EmployeeID == id);
+            
+        }
+    }
+}
+    }
